a9a9bece8d832be89877ad3cfaef7c1c697837fa
mbusenitz <mbusenitz@gmail.com>
2015-02-10 21:45:14 -0800

    master building

---
+++ test/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+/*
+
+void run_test() 
+{
+	typedef functor< dynamic_policy< char*(*)() > > functor_type;
+	functor_type create( &fuck_you );		
+	object_factory< int, functor_type > of;
+	of.register_create( 3, create );
+	cout << string( of.create( 3 ) ) << std::endl; 
+}
+
+*/
+
+int main(int argc, const char * argv[])
+{
+	std::cout << "mofo" << std::endl;
+	return 0;
+}
---
+++ test/test.gyp
@@ -0,0 +1,18 @@
+{
+	'includes':[
+		'../spawn.gypi',
+		'plank/def/mac-targets.gypi',
+		'plank/def/cpp11.gypi'
+	],#includes
+	'target_defaults': {
+		'target_name': 'test', 
+		'type': 'executable',
+		'sources': [
+			'../test/src/main.cpp'
+		], #sources
+		'include_dirs': [
+			'plank/src/',
+			'../test/'
+		], #include_dirs		
+	}, #target_defaults
+}
