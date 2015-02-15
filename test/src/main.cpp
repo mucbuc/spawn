4a25863ba123fcff93ecf97f1790ef26464280e6
mbusenitz <mbusenitz@gmail.com>
2015-02-15 11:54:27 -0800

    dev abstract test

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -8,48 +8,66 @@
 #include "../../src/abstract_factory.h"
 #include "../../src/concrete_factory.h"
 
-const char * fuck_you()
-{
-	static std::string local( "hello" ); 
-	return local.c_str();
-}
-
-void run_test() 
-{
-	using namespace std;
-	typedef function< const char * () > functor_type;
-	
-	functor_type create( & fuck_you );		
-	om636::object_factory< int, functor_type > off;
-	off.register_create( 3, create );
-
-	ASSERT( !strcmp(off.create( 3 ), "hello") );
-}
-
 struct Abstract1
 {
-	
+ 	virtual const char * hello() = 0;
 };
 
 struct Abstract2
 {
+ 	virtual const char * hello() = 0;
 };
 
-int main(int argc, const char * argv[])
+struct Concrete1 : Abstract1
+{	
+	const char * hello()
+	{
+		return "hello";
+	}
+};
+
+struct Concrete2 : Abstract2
+{
+	const char * hello()
+	{
+		return 0;
+	}
+};
+
+void run_abstract_factory_test()
 {
 	using namespace om636;
-
-	run_test();
-
 	typedef abstract_factory< om636::abstract_unit, Abstract1, Abstract2 > abstract_type;
 	typedef concrete_factory< 
 		abstract_type, 
-		std::tuple< Abstract1, Abstract1>,
-		std::tuple< Abstract2, Abstract2> > 
+		std::tuple< Concrete1, Abstract1>,
+		std::tuple< Concrete2, Abstract2> > 
 	concrete_type;
 	
 	concrete_type factory; 
-	factory.create<Abstract1>();
+	ASSERT( !factory.create<Abstract2>()->hello() );
+	ASSERT( !strcmp(factory.create<Abstract1>()->hello(), "hello") );
+}
+
+void run_object_factory_test() 
+{
+	using namespace std;
+	typedef function< const char * () > functor_type;
+	
+	om636::object_factory< int, functor_type > off;
+	off.register_create( 3, [](){
+		return "hello";
+	});
+
+	ASSERT( !strcmp(off.create( 3 ), "hello") );
+}
+
+
+int main(int argc, const char * argv[])
+{
+
+	run_object_factory_test();
+	run_abstract_factory_test();
 
 	return 0;
 }
